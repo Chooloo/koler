9c434cefd3f64f7689690a044addb8190f976d9f
roei <roeiedr5@gmail.com>
2018-12-30 00:33:39 +0200

    When call has ended, just click on the screen to close the screen

--- app/src/main/java/com/chooloo/www/callmanager/activity/OngoingCallActivity.java
+++ app/src/main/java/com/chooloo/www/callmanager/activity/OngoingCallActivity.java
@@ -5,6 +5,7 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -14,10 +15,13 @@ import com.chooloo.www.callmanager.service.CallManager;
 import com.chooloo.www.callmanager.service.CallService;
 
 public class OngoingCallActivity extends AppCompatActivity {
-
+    // TODO 1 make a list of mSTATUS options
+    // TODO 2 try to make a click listener when the call has ended to close the app
     @BindView(R.id.answerBtn) Button answerBtn;
     @BindView(R.id.denyBtn) Button denyBtn;
     @BindView(R.id.placeholder) TextView statusTxt;
+
+    static String mSTATUS = "INCOMING";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,11 +29,22 @@ public class OngoingCallActivity extends AppCompatActivity {
         setContentView(R.layout.activity_ongoing_call);
         ButterKnife.bind(this);
 
+        View.OnTouchListener Clicked = new View.OnTouchListener(){
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(mSTATUS == "ENDED"){
+                    System.exit(0);
+                }
+                return true;
+            }
+        };
+
         answerBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 CallManager.acceptCall();
                 statusTxt.setText("Call In Progress");
+                mSTATUS = "PROGRESS";
             }
         });
 
@@ -38,6 +53,7 @@ public class OngoingCallActivity extends AppCompatActivity {
             public void onClick(View v) {
                 CallManager.denyCall();
                 statusTxt.setText("Call Ended");
+                mSTATUS = "ENDED";
             }
         });
 
