a41888ccdfd4ffb47e8d86509908a481323362c3
Ido Fang Bentov <dodobentov@gmail.com>
2018-12-31 17:26:39 +0200

    Change MainActivity to contain a settings fragment

--- app/src/main/java/com/chooloo/www/callmanager/activity/MainActivity.java
+++ app/src/main/java/com/chooloo/www/callmanager/activity/MainActivity.java
@@ -6,7 +6,15 @@ import android.telecom.TelecomManager;
 
 import com.chooloo.www.callmanager.R;
 
+import androidx.fragment.app.Fragment;
+import androidx.preference.ListPreference;
+import androidx.preference.PreferenceFragmentCompat;
+
 public class MainActivity extends ToolbarActivity {
+
+    private static final String TAG_FRAGMENT = "fragment";
+
+    private Fragment mFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +27,27 @@ public class MainActivity extends ToolbarActivity {
         if (!getSystemService(TelecomManager.class).getDefaultDialerPackage().equals(packageName)) {
             startActivity(new Intent(TelecomManager.ACTION_CHANGE_DEFAULT_DIALER)
                     .putExtra(TelecomManager.EXTRA_CHANGE_DEFAULT_DIALER_PACKAGE_NAME, packageName));
+        }
+
+        mFragment = new SettingsFragment();
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment, mFragment, TAG_FRAGMENT)
+                .commit();
+    }
+
+    public static class SettingsFragment extends PreferenceFragmentCompat {
+
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            setPreferencesFromResource(R.xml.preference, rootKey);
+
+            //Init preferences
+            ListPreference endCallTimerPref = (ListPreference) findPreference(getString(R.string.pref_end_call_timer_key));
+            endCallTimerPref.setSummary(endCallTimerPref.getEntry());
+            endCallTimerPref.setOnPreferenceChangeListener((preference, newValue) -> {
+                endCallTimerPref.setSummary(endCallTimerPref.getEntry());
+                return true;
+            });
         }
     }
 }
