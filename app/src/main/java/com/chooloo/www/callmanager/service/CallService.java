6c251dc4c4ef0e029c45e2e25347f2f44e92cc4a
roei <roeiedr5@gmail.com>
2019-01-05 15:30:16 +0200

    Added CallbackService.java

--- app/src/main/java/com/chooloo/www/callmanager/service/CallService.java
+++ app/src/main/java/com/chooloo/www/callmanager/service/CallService.java
@@ -12,41 +12,10 @@ import com.chooloo.www.callmanager.activity.OngoingCallActivity;
 
 public class CallService extends InCallService {
 
-    public static Context mContext;
-    TelephonyManager mTelephonyManager = (TelephonyManager)getApplicationContext().getSystemService(TELEPHONY_SERVICE);
-
-    public PhoneStateListener mPhoneStateListener = new PhoneStateListener(){
-
-        @Override
-        public void onCallStateChanged(int state, String phoneNumber) {
-            CallManager.sPhoneNumber = phoneNumber;
-            switch (state){
-                case TelephonyManager.CALL_STATE_IDLE:
-                    CallManager.sCallState = "IDLE";
-                    break;
-                case TelephonyManager.CALL_STATE_RINGING:
-                    CallManager.sCallState = "RINGING";
-                    break;
-                case TelephonyManager.CALL_STATE_OFFHOOK:
-                    CallManager.sCallState = "OFFHOOK";
-                    break;
-            }
-        }
-    };
-
     @Override
     public void onCallAdded(Call call) {
-        mTelephonyManager.listen(mPhoneStateListener,
-                PhoneStateListener.LISTEN_CALL_STATE
-                | PhoneStateListener.LISTEN_CELL_INFO // Requires API 17
-                | PhoneStateListener.LISTEN_CELL_LOCATION
-                | PhoneStateListener.LISTEN_DATA_ACTIVITY
-                | PhoneStateListener.LISTEN_DATA_CONNECTION_STATE
-                | PhoneStateListener.LISTEN_SERVICE_STATE
-                | PhoneStateListener.LISTEN_SIGNAL_STRENGTHS
-                | PhoneStateListener.LISTEN_CALL_FORWARDING_INDICATOR
-                | PhoneStateListener.LISTEN_MESSAGE_WAITING_INDICATOR);
         super.onCallAdded(call);
+        call.registerCallback(new CallbackService(this));
         startActivity(new Intent(this, OngoingCallActivity.class));
         CallManager.sCall = call;
     }
