8e50cf29fc87299a2945ab663c161cf36b793dc9
roei <roeiedr5@gmail.com>
2019-01-25 19:10:57 +0200

    More ui, need it online lol

--- app/src/main/java/com/chooloo/www/callmanager/activity/MainActivity.java
+++ app/src/main/java/com/chooloo/www/callmanager/activity/MainActivity.java
@@ -13,6 +13,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.view.inputmethod.InputMethodManager;
@@ -65,6 +67,9 @@ public class MainActivity extends ToolbarActivity {
         setContentView(R.layout.activity_main);
         PreferenceUtils.getInstance(this);
         ButterKnife.bind(this);
+
+        // It's exactly what it sounds like
+        makeBarTransparent();
 
         // Init timber
         Timber.plant(new Timber.DebugTree());
@@ -232,6 +237,19 @@ public class MainActivity extends ToolbarActivity {
                 Timber.e(e, "Couldn't call %s", sToNumber);
             }
         }
+    }
+
+    public void makeBarTransparent() {
+        Window window = this.getWindow();
+
+        // clear FLAG_TRANSLUCENT_STATUS flag:
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+        // add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+
+        // finally change the color
+        window.setStatusBarColor(ContextCompat.getColor(this, R.color.grey_back));
     }
 
     /**
