c1f3e42c7444abf58883ecd60d29c189cb3fa16d
Ido Fang Bentov <dodobentov@gmail.com>
2019-05-22 12:24:56 +0300

    Add about activity

---
+++ app/src/main/java/com/chooloo/www/callmanager/ui/activity/AboutActivity.java
@@ -0,0 +1,16 @@
+package com.chooloo.www.callmanager.ui.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.chooloo.www.callmanager.R;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
