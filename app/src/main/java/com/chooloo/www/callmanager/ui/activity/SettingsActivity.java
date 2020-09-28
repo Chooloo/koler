4745b25312b86b9648328c1607baef327d57c5f5
Roei Edri <roeiedr5@gmail.com>
2020-09-28 18:14:00 +0300

    Added contact activity files

--- app/src/main/java/com/chooloo/www/callmanager/ui/activity/SettingsActivity.java
+++ app/src/main/java/com/chooloo/www/callmanager/ui/activity/SettingsActivity.java
@@ -1,32 +1,14 @@
 package com.chooloo.www.callmanager.ui.activity;
 
-import android.annotation.SuppressLint;
-import android.content.Intent;
 import android.os.Bundle;
-import android.telephony.SubscriptionInfo;
-import android.telephony.SubscriptionManager;
-import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.preference.CheckBoxPreference;
-import androidx.preference.ListPreference;
-import androidx.preference.Preference;
-import androidx.preference.PreferenceFragmentCompat;
-import androidx.preference.SwitchPreference;
 
 import com.chooloo.www.callmanager.R;
 import com.chooloo.www.callmanager.ui.fragment.SettingsFragment;
-import com.chooloo.www.callmanager.util.PermissionUtils;
 import com.chooloo.www.callmanager.util.ThemeUtils;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
-
-import timber.log.Timber;
-
-import static android.Manifest.permission.READ_PHONE_STATE;
 
 //TODO add icons
 public class SettingsActivity extends AbsThemeActivity {
