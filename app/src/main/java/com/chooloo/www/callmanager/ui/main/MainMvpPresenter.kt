8275327813179bb74b531981fcee4b3ad20d556d
Roei Edri <roeiedr5@gmail.com>
2021-02-03 00:01:20 +0200

    Converted main activity to kotlin

---
+++ app/src/main/java/com/chooloo/www/callmanager/ui/main/MainMvpPresenter.kt
@@ -0,0 +1,16 @@
+package com.chooloo.www.callmanager.ui.main
+
+import android.content.Intent
+import android.view.MenuItem
+import com.chooloo.www.callmanager.ui.base.MvpPresenter
+
+interface MainMvpPresenter<V : MainMvpView?> : MvpPresenter<V> {
+    fun onDialpadFabClick()
+    fun onSearchTextChanged(text: String)
+    fun onSearchFocusChanged(isFocused: Boolean)
+    fun onDialNumberChanged(number: String)
+    fun onOptionsItemSelected(item: MenuItem)
+    fun onBackPressed(): Boolean
+    fun onMenuClick()
+    fun onViewIntent(intent: Intent)
+}
---
+++ app/src/main/java/com/chooloo/www/callmanager/ui/main/MainPresenter.kt
@@ -0,0 +1,53 @@
+package com.chooloo.www.callmanager.ui.main
+
+import android.content.Intent
+import android.view.MenuItem
+import com.chooloo.www.callmanager.R
+import com.chooloo.www.callmanager.ui.base.BasePresenter
+import java.io.UnsupportedEncodingException
+import java.net.URLDecoder
+
+class MainPresenter<V : MainMvpView?> : BasePresenter<V>(), MainMvpPresenter<V> {
+    override fun onDialpadFabClick() {
+        mvpView?.showDialpad(true)
+    }
+
+    override fun onDialNumberChanged(number: String) {}
+    override fun onSearchTextChanged(text: String) {}
+    override fun onSearchFocusChanged(isFocused: Boolean) {}
+
+    override fun onOptionsItemSelected(item: MenuItem) {
+        when (item.itemId) {
+            R.id.action_settings -> mvpView?.goToSettings()
+            R.id.action_about -> mvpView?.goToAbout()
+        }
+    }
+
+    override fun onBackPressed(): Boolean {
+        if (mvpView?.isDialpadShown == true) {
+            mvpView?.showDialpad(false)
+            return true
+        }
+        return false
+    }
+
+    override fun onMenuClick() {
+        mvpView?.showMenu(true)
+    }
+
+    override fun onViewIntent(intent: Intent) {
+        val intentText = try {
+            URLDecoder.decode(intent.dataString, "utf-8")
+        } catch (e: UnsupportedEncodingException) {
+            mvpView?.showError("An error occured when trying to get phone number :(")
+            return
+        }
+
+        if (intentText.contains("tel:")) {
+            mvpView?.showDialpad(true)
+            mvpView?.dialpadNumber = intentText
+        } else {
+            mvpView?.showError("No phone number detected")
+        }
+    }
+}
