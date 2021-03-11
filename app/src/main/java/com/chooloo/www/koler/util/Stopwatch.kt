c30ff8f785cd8f440358d37e769e1090c31e35be
Roei Edri <roeiedr5@gmail.com>
2021-03-11 21:41:27 +0200

    Removed junk files

--- app/src/main/java/com/chooloo/www/koler/util/Stopwatch.kt
+++ app/src/main/java/com/chooloo/www/koler/util/Stopwatch.kt
@@ -10,37 +10,22 @@ class Stopwatch {
     private var stopTime: Long = 0
     private var isRunning = false
 
-    /**
-     * Elapsed time in milliseconds
-     */
     val elapsedTime: Long
         get() = if (isRunning) System.currentTimeMillis() - startTime else stopTime - startTime
 
-    /**
-     * Elapsed time in seconds
-     */
     val elapsedTimeSecs: Long
         get() = if (isRunning) (System.currentTimeMillis() - startTime) / 1000 else (stopTime - startTime) / 1000
 
-    /**
-     * Starts the timer
-     */
     fun start() {
         startTime = System.currentTimeMillis()
         isRunning = true
     }
 
-    /**
-     * Stops the timer
-     */
     fun stop() {
         stopTime = System.currentTimeMillis()
         isRunning = false
     }
 
-    /**
-     * Returns the time in a string format
-     */
     val stringTime: String
         get() {
             val currentTime = elapsedTime
