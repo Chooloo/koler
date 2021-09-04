5eb00944b5ffbc10770049b5d92a1d15d9e6b07d
Roei Edri <roeiedr5@gmail.com>
2021-09-04 17:26:11 +0300

    Fixed contacts search crashes when no permission #254

--- app/src/main/java/com/chooloo/www/koler/ui/list/ListPresenter.kt
+++ app/src/main/java/com/chooloo/www/koler/ui/list/ListPresenter.kt
@@ -6,6 +6,8 @@ abstract class ListPresenter<ItemType, V : ListContract.View<ItemType>>(view: V)
     BasePresenter<V>(view),
     ListContract.Presenter<ItemType, V> {
 
+    private var _permissionsGranted = false
+
     override fun onStart() {
         boundComponent.permissionInteractor.runWithPermissions(
             permissions = this.requiredPermissions,
@@ -34,12 +36,15 @@ abstract class ListPresenter<ItemType, V : ListContract.View<ItemType>>(view: V)
     }
 
     override fun onPermissionsGranted() {
+        _permissionsGranted = true
         view.emptyStateText = noResultsMessage
         observeData()
     }
 
     override fun onSearchTextChanged(text: String) {
-        applyFilter(text)
+        if (_permissionsGranted) {
+            applyFilter(text)
+        }
     }
 
     override fun onDataChanged(items: ArrayList<ItemType>) {
