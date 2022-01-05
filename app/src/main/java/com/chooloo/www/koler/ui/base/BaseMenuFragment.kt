f58cc77cbb8f00ff43cec16b24fed119b82745f3
Roei Edri <roeiedr5@gmail.com>
2022-01-05 19:29:29 +0200

    Made basemenufragment useable as standalone

--- app/src/main/java/com/chooloo/www/koler/ui/base/BaseMenuFragment.kt
+++ app/src/main/java/com/chooloo/www/koler/ui/base/BaseMenuFragment.kt
@@ -8,7 +8,8 @@ import com.chooloo.www.koler.adapter.MenuAdapter
 import com.chooloo.www.koler.databinding.MenuBinding
 
 @SuppressLint("RestrictedApi")
-abstract class BaseMenuFragment : BaseFragment() {
+open class BaseMenuFragment : BaseFragment() {
+    private var _onMenuItemClickListener: (MenuItem) -> Unit = {}
     private val adapter by lazy { MenuAdapter(component) }
     private val binding by lazy { MenuBinding.inflate(layoutInflater) }
 
@@ -16,7 +17,10 @@ abstract class BaseMenuFragment : BaseFragment() {
 
 
     override fun onSetup() {
-        adapter.setOnItemClickListener(::onMenuItemClick)
+        adapter.setOnItemClickListener {
+            _onMenuItemClickListener.invoke(it)
+            onMenuItemClick(it)
+        }
         binding.menuRecyclerView.adapter = adapter
     }
 
@@ -29,6 +33,10 @@ abstract class BaseMenuFragment : BaseFragment() {
         setMenu(MenuBuilder(baseActivity).apply {
             MenuInflater(baseActivity).inflate(menuRes, this)
         })
+    }
+
+    fun setOnMenuItemClickListener(onMenuItemClickListener: (menuItem: MenuItem) -> Unit) {
+        _onMenuItemClickListener = onMenuItemClickListener
     }
 
     protected open fun onMenuItemClick(menuItem: MenuItem) {}
